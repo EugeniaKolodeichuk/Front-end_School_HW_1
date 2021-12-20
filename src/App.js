f532f6b3090765a6113bb63cb89f7889acf9956f
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2021-12-20 22:10:08 +0200

    delete unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 /* eslint-disable func-call-spacing */
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router';
-
 import './App.css';
 import Loader from './components/Loader/Loader';
-/* import AppBar from './components/AppBar/AppBar';
-import Container from './components/Container/Container';
-import NewsView from './views/NewsView';
-import NotFoundView from './views/NotFoundView';
-import ProfileView from './views/ProfileView'; */
-/* import Spinner from './components/Loader/Loader'; */
 
 const AppBar = lazy(() =>
   import('./components/AppBar/AppBar' /*webpackChunkName: "app-bar"*/),
@@ -20,10 +13,6 @@ const Container = lazy(() =>
 const NewsView = lazy(() =>
   import('./views/NewsView/NewsView' /*webpackChunkName: "news-view"*/),
 );
-/* const NotFoundView = lazy(() =>
-  import(
-    './views/NotFoundView/NotFoundView' т),
-); */
 const ProfileView = lazy(() =>
   import(
     './views/ProfileView/ProfileView' /*webpackChunkName: "profile-view"*/
@@ -41,13 +30,6 @@ export default function App() {
             <ProfileView />
           </Route>
 
-          {/* <Route path="/profile">
-              <NotFoundView />
-            </Route> */}
-
-          {/* <Route path="/">
-              <NewsView />
-            </Route> */}
           <Route>
             <NewsView />
           </Route>
