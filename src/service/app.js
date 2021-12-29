41cce5b8d46a7812bb0c7ca5d009f51fadbabe00
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2021-12-29 11:56:21 +0200

    Update app.js

--- src/service/app.js
+++ src/service/app.js
@@ -14,25 +14,3 @@ export default axios.create({
 export function getTrends(currentPage) {
   return axios.get(`/trending/feed/?limit=30&page=${currentPage}`);
 }
-
-/* export function getUserInfo(username) {
-  return axios.get(`/user/info/${username}`);
-}
-
-export function getUserFeed() {
-  return axios.get('/trending/feed');
-} */
-
-/* import { useState } from 'react';
-
-const [currentPage] = useState(1);
-
-const getTrends = {
-  method: 'GET',
-  url: `https://tiktok33.p.rapidapi.com/trending/feed/?limit=30&page=${currentPage}`,
-  headers: {
-    'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-    'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66',
-  },
-};
-export default getTrends; */
